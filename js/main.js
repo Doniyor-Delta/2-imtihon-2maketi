9a9ea851e891ebb87e26e2d4c4aaea8ce78ea183
Doniyor-Delta <doniyordelta9090@gmail.com>
2022-02-09 01:19:47 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const burgerToggler = document.querySelector(".header__burger");
 const navigation = document.querySelector(".header__nav");
 
-
 burgerToggler.addEventListener("click", function() {
     if (!burgerToggler.classList.contains("header__burger--opened")){
         burgerToggler.classList.add("header__burger--opened");
@@ -15,15 +14,19 @@ burgerToggler.addEventListener("click", function() {
 
 navigation.addEventListener("animationend", function(evt) {
     if (evt.animationName === "nav-close")
+    navigation.classList.remove("header__nav--open");
     navigation.classList.remove("header__nav--closing");
 
 
 });
 
+
 const buttonToggler = document.querySelector(".faqs__button");
 const textToggler = document.querySelector(".faqs__button-description");
+
 
 buttonToggler.addEventListener("click", function() {
 
     textToggler.classList.toggle("visually-hidden");
 });
+
